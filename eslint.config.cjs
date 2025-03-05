94ae8198835a0d87a1328ef12ed6d2d3e641463b
ParasSolanki <parassolanki151@gmail.com>
2025-03-05 18:29:57 +0530

    feat: add neverthrow

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,10 +1,15 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import neverThrow from "eslint-plugin-neverthrow";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...neverThrow.configs.recommended,
+    ],
     languageOptions: {
       ecmaVersion: 2022,
     },
